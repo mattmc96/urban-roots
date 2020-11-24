65e1c8b2ece7bf3be5ccfcfab47b1b9c97f708d7
Tessa Woodard <tessawoodard@outlook.com>
2020-11-23 23:27:15 -0500

    wip

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -30,10 +30,33 @@ class Shop extends React.Component {
   displayProducts = (products) => {
     return products.map((product, index) => (
       <div key={index} className="product-display">
-        <h3>{product.name}</h3>
-        <p>${product.price}</p>
-        <img src={product.image} alt="plant-img" style={{ width: '20%' }} />
+        <title>Harvest vase</title>
+        <div className="wrapper">
+          <div className="product-img">
+            <img src={product.image} alt="plant-img" height="420" />
+          </div>
+          <div className="product-info">
+            <div className="product-text">
+              <h1>{product.name}</h1>
+              <h2>by Urban Roots</h2>
+              <p>
+                Harvest Vases are a reinterpretation
+                <br /> of peeled fruits and vegetables as
+                <br /> functional objects. The surfaces
+                <br /> appear to be sliced and pulled aside,
+                <br /> allowing room for growth.
+              </p>
+            </div>
+            <div className="product-price-btn">
+              <p>
+                <span>${product.price}</span>
+              </p>
+              <button type="button">buy now</button>
+            </div>
+          </div>
+        </div>
       </div>
+      //   </div>
     ))
   }
   render() {
