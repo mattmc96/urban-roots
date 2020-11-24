e6ccaef7151209e03d69c30d7123476147ad4194
Tessa Woodard <tessawoodard@outlook.com>
2020-11-23 20:04:17 -0500

    wip

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import './login.css'
 
-export const Login = () => {
-    return (
-        <div>
-            this is a div
-        </div>
-    )
+function Login() {
+  return (
+    <div className="login_input_container">
+      <p>Email:</p>
+      <input
+        maxLength="100"
+        placeholder="Enter Email"
+        name="email"
+        onChange={(e) => {
+          this.handleInput(e)
+        }}
+      />
+      <p>Password:</p>
+      <input
+        type="password"
+        maxLength="20"
+        placeholder="Enter Password"
+        name="password"
+        onChange={(e) => {
+          this.handleInput(e)
+        }}
+      />
+      <button
+        onClick={() => {
+          this.handleLogin()
+        }}
+      >
+        Login
+      </button>
+    </div>
+  )
 }
 
 export default Login
